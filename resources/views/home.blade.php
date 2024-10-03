7682e1cdd82c439ddea0642e7ccab8a5180dc8ee
Muhamad Gunawan <igunnawan24@gmail.com>
2024-10-03 15:44:42 +0700

    tambahan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,6 +89,19 @@
             </div>  
         </div>
     </section>
+
+    <section id="WhatWeProvide" class="pb-20">
+        {{-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,256L48,218.7C96,181,192,107,288,80C384,53,480,75,576,112C672,149,768,203,864,224C960,245,1056,235,1152,192C1248,149,1344,75,1392,37.3L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> --}}
+        <div class="w-full min-h-screen bg-gradient-to-b from-[#A47EFD] to-[#FF88E6]">
+            <div class="container mx-auto pt-16 px-4 lg:px-8">
+                <div class="flex flex-wrap">
+                    <div class="mb-10 flex">
+                        <h1 class="flex text-center items-center text-4xl font-bold bg-white text-transparent bg-clip-text">Our Product</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     
 </body>
 </html>
