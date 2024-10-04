c83e262fe6cd111cddb4784637b2efbaa9b6edc4
Baa-bii <alba6259@gmail.com>
2024-10-04 20:21:24 +0700

    ganti kata2 dikit

--- resources/views/createyourown.blade.php
+++ resources/views/createyourown.blade.php
@@ -13,9 +13,9 @@
           <svg class="h-6 w-6 group-hover:stroke-pink-400" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
           </svg>
-          <h3 class="text-slate-700 group-hover:text-pink-300 text-sm font-semibold">New project</h3>
+          <h3 class="text-slate-700 group-hover:text-pink-300 text-sm font-semibold">New Design</h3>
         </div>
-        <p class="text-slate-700 group-hover:text-pink-300 text-sm">Create a new project from a variety of starting templates.</p>
+        <p class="text-slate-700 group-hover:text-pink-300 text-sm">Create a new design from a variety of starting templates.</p>
       </a>
 </body>
 </html>
