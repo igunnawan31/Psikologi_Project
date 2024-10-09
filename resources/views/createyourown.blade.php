97c6299f632aaa14d6ba381fa2a2c75e1fc14db3
Baa-bii <alba6259@gmail.com>
2024-10-09 16:52:41 +0700

    ngubah bentuk createyourown

--- resources/views/createyourown.blade.php
+++ resources/views/createyourown.blade.php
@@ -11,16 +11,16 @@
     <section id="Home" class="pt-36 pb-36">
       <div class="container mx-auto">
           <div class="flex justify-center items-center text-center">
-              <h2 class="text-5xl mx-auto font-bold h-20 bg-gradient-to-b from-[#A47EFD] to-[#FF88E6] text-transparent bg-clip-text  ">
+              <h2 class="text-5xl mx-auto font-bold my-10 bg-gradient-to-b from-[#A47EFD] to-[#FF88E6] text-transparent bg-clip-text  ">
                   Create Your Own Greeting Cards!
               </h2>
           </div>
       </div>
   </section>
-    <div class="container-mx-auto"><a href="#" class="group block m-10 max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover: bg-gradient-to-b from-[#C0E9ED80] to-[#FBB6C880] hover:ring-pink-400">
+    <div class="container-mx-auto"><a href="#" class="group block m-10 max-w-sm mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 border-dashed border-2 border-pink-300 hover: bg-gradient-to-b from-[#C0E9ED80] to-[#FBB6C880] hover:ring-pink-400 hover:border-none">
         <div class="flex items-center space-x-3">
-          <svg class="h-6 w-6 group-hover:stroke-pink-400" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+          <svg class="h-6 w-6 group-hover:stroke-pink-300" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4z"></path>
+            <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path></svg>
           </svg>
           <h3 class="text-slate-700 group-hover:text-pink-300 text-sm font-semibold">New Design</h3>
         </div>
